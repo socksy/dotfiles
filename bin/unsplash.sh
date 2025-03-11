4e00e006848b6895ad36d8f82fa53ef63e363909
Ben Lovell <ben.j.lovell@gmail.com>
2025-03-11 21:31:14 +0100

    Commit the scripts for the sake of script

---
+++ bin/unsplash.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+pushd "$(dirname $0)" > /dev/null
+ACCESS_KEY="$(cat _secret_unsplash_api_access_key | tr -d '\n ' )"
+SECRET_KEY="$(cat _secret_unsplash_api_secret_key | tr -d '\n ' )"
+popd > /dev/null
+
+if [ -z "$@" ]; then
+  QUERY_STRING=""
+else
+  QUERY_STRING="&query=$@"
+fi
+
+photo_json="$(curl -s -XGET -H "Authorization: Client-ID $ACCESS_KEY" "https://api.unsplash.com/photos/random?orientation=landscape&client_id=$ACCESS_KEY$QUERY_STRING")"
+echo $photo_json
+url="$(echo $photo_json | jq -r '.urls.raw')"
+slug="$(echo $photo_json | jq -r '.slug')"
+
+curl -s $url > $slug && echo $PWD/$slug
